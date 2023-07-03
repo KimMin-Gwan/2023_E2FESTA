22b727e17b9373283e98efd41bfea564a02fc3a7
sunjuwhan <mathsun0011@gmail.com>
2023-07-03 15:31:25 +0900

    Update bluetoothScan_0701_2130.py
    신호등 check

--- develop/Tests/juwhan_test/bluetoothScan_0701_2130.py
+++ develop/Tests/juwhan_test/bluetoothScan_0701_2130.py
@@ -15,6 +15,7 @@
 * JH SUN            2023.07.02      V1.02       우선순위큐에서 데이터 추출후 원소 초기화 작업                                                    시작할때 오류 발생(1회) 
 * JH SUN            2023 07.02      V1.10       멀티 스레드를 통해 scan과 출력을 각각의 스레드로 관리한다.                                       시작할때 오류 발생(1회)
 * JH SUN            2023 07.02      V1.11       dead_lock 발생 해결 priorty_queue에서는 que.notempty()가아닌 not que.empty()사용   멀티스레드 정상 작동                                                      시작할때 오류 발생(1회)
+* JH SUN            2023 07.03      V1.20       ReceiveSignal 클래스 생성                                                                      
 """
 from bluepy.btle import Scanner, DefaultDelegate
 from queue import PriorityQueue
@@ -41,10 +42,12 @@ class ScanDelegate(DefaultDelegate):
 
 class ReceiveSignal:
     def __init__(self,scanner,duration):
-        self.scanner=scanner
-        self.duration=duration
+        self.scanner=scanner  #scanner
+        self.duration=duration  #scan duration
         self.lock=threading.Lock()
         self.que=PriorityQueue()
+        self.data=''
+
     def scanData(self):   #scan thread func
         while True:
             devices = self.scanner.scan(self.duration)
@@ -61,6 +64,9 @@ class ReceiveSignal:
                         self.lock.release()
             time.sleep(1)
 
+
+
+
     def print_scan_data(self):    #print thread func
         while True:
             self.lock.acquire()
@@ -69,11 +75,22 @@ class ReceiveSignal:
                 time.sleep(2)
             else:
                 rssi_beacon,data=self.que.get()
-                print("Nearest beacon_rssi : ",rssi_beacon,"beacon_data: ",data)
+                self.data=data
+                #print("Nearest beacon_rssi : ",rssi_beacon,"beacon_data: ",data)
+                self.check_flag()
                 while not self.que.empty():  #priortyqueue use not que.empty()  erase all value 
                     self.que.get()
                 self.lock.release()
                 time.sleep(1)
+
+    def check_flag(self):
+        if self.data in "74 72 61 66 66 69 63 ":
+            color,Ten,One=self.data[7:8],self.data[8:9],self.data[9:10]
+            print("This is Traffic sign, color : ",color,"left time is ",Ten-30,One-30)
+        
+    def receive_traffic_sign():
+        pass
+
 
 
 
@@ -82,6 +99,8 @@ def main():
     scan_delegate = ScanDelegate()
     scanner = Scanner().withDelegate(scan_delegate)
     receive_signal=ReceiveSignal(scanner,duration)
+
+
     scan_thread=threading.Thread(target=receive_signal.scanData)
     print_thread=threading.Thread(target=receive_signal.print_scan_data)
 
