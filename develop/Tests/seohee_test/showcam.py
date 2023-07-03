55c64d3372f6e5ce51c95fdb309b1ecd9a4b77cd
seohee <shp6788@naver.com>
2023-07-03 19:32:31 +0900

    show_cam2

--- develop/Tests/seohee_test/showcam.py
+++ develop/Tests/seohee_test/showcam.py
@@ -4,8 +4,15 @@ import cv2
 
 app=Flask(__name__)
 
-video_path=""
-cap=cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture(0)
+
+width=int(cap.get(3))
+height=int(cap.get(4))
+fps=20
+
+fcc=cv2.VideoWriter_fourcc('M','J','P','G')
+out=cv2.VideoWriter('webcam.avi',fcc,fps,(width,height),isColor=False)
+print(out.isOpened())
 
 @app.route('/')
 def video_show():
@@ -13,22 +20,24 @@ def video_show():
 
 def gen_frames():
     while True:
-        _, frame=cap.read()
-        if not _:
+       ret,frame=cap.read()
+    if ret:
+        gray=cv2.cvtColor(frame,cv2.COLOR_BGR2BGR)
+        out.write(gray)
+        cv2.imshow('frame',gray)
+
+        if cv2.waitKey(1)&0xFF==ord('1'): 
             break
-        else:
-            results=model(frame)
-            annotated_frame=results.render()
+    else:
+        print("Fail to read frame!")
+        break
 
-            ret, buffer=cv2.imencodde('.jpg', frame)
-            frmae=buffer.tobytes()
-
-        yield(b'--frame\r\n'
-              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+     yield(b'--frame\r\n'
+        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video')
 def video():
-    return Respoonse(gen_frames(), mimetype='multipart/x-mized-replace;boundary=frame')
+    return Respoonse(gen_frames(), mimetype='multipart/x-mized-replace; boundary=frame')
 
 if __name__=='__main__':
     app.run(host='165.229.125.90',port=7777)
