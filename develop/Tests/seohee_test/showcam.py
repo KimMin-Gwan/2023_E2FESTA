3af793e8cf6ad222762c8769b28270f40b6a7758
seohee <shp6788@naver.com>
2023-07-03 19:42:37 +0900

    show_cam

--- develop/Tests/seohee_test/showcam.py
+++ develop/Tests/seohee_test/showcam.py
@@ -1,45 +1,23 @@
-#test_show_camera
-from flask import Flask, render_template, Respoonse
-import cv2
 
-app=Flask(__name__)
+from flask import Flask, render_template, Response
+from camera import Camera
 
-cap = cv2.VideoCapture(0)
-
-width=int(cap.get(3))
-height=int(cap.get(4))
-fps=20
-
-fcc=cv2.VideoWriter_fourcc('M','J','P','G')
-out=cv2.VideoWriter('webcam.avi',fcc,fps,(width,height),isColor=False)
-print(out.isOpened())
+app = Flask(__name__)
 
 @app.route('/')
-def video_show():
-    return render_template('video_show.html')
+def index():
+   return render_template('video_show.html')
 
-def gen_frames():
-   while(True):
-        ret,frame=cap.read()
-        if ret:
-            gray=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            out.write(gray)
-            cv2.imshow('frame',gray)
+def gen(camera):
+   while True:
+       frame = camera.get_frame()
+       yield (b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-            if cv2.waitKey(1)&0xFF==ord('1'): break
-        else:
-            print("Fail to read frame!")
-            break
+@app.route('/video_feed')
+def video_feed():
+   return Response(gen(Camera()),
+                   mimetype='multipart/x-mixed-replace; boundary=frame')
 
-        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-    
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-
-@app.route('/video')
-def video():
-    return Respoonse(gen_frames(), mimetype='multipart/x-mized-replace; boundary=frame')
-
-if __name__=='__main__':
-    app.run(host='165.229.125.90',port=7777)
+if __name__ == '__main__':
+   app.run(host='165.229.125.90',port=7777)
