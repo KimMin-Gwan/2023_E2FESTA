014eecd8dd6c8a25ce6fcd258eb4430ffdd3134d
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2023-07-04 07:18:39 -0400

    Update broadcasting_SUB.py

--- develop/Tests/jaehyun_test/beacon/broadcasting_SUB.py
+++ develop/Tests/jaehyun_test/beacon/broadcasting_SUB.py
@@ -1,17 +1,14 @@
 """
-* Project : 2023CDP Eddystone Broadcasting
+* Project : 2023CDP Eddystone Broadcasting Subway ver
 * Program Purpose and Features :
 * - send broadcasting message
 * Author : JH KIM, JH SUN
-* First Write Date : 2023.07.03
+* First Write Date : 2023.07.04
 * ==========================================================================
 * Program history
 * ==========================================================================
 * Author    		Date		    Version		History
-* JH KIM            2023.07.02      v1.00       First Write
-* JH KIM            2023.07.03      v1.01       add init code
-* JH KIM            2023.07.03      v1.02       creat class
-* JH KIM            2023.07.04      v1.03       TRF code modified
+* JH KIM            2023.07.04      v1.00       First Write
 """
 import os
 import time
@@ -21,8 +18,8 @@ class trafficSignal:
     def __init__(self):
         os.system("sudo hciconfig hci0 up")
         os.system("sudo hciconfig hci0 leadv 3")
-        self.signal = "R"
-        self.leftTime = 60
+        self.signal = "U"
+        self.leftTime = 6
 
     def setSignal(self, newSig):
         self.signal = newSig
@@ -32,27 +29,28 @@ class trafficSignal:
 
     def trafficBroadcasting(self):
         defaultStr = "sudo hcitool -i hci0 cmd 0x08 0x0008 17 02 01 06 03 03 aa fe 0f 16 aa fe 10 00 "
-        TRF = "54 52 46 "
-        if self.getSignal() == "G":
-            signalStr = "47 "
+        SUB = "53 55 42 "
+        if self.getSignal() == "U":
+            signalStr = "55 "
         else:
-            signalStr = "52 "
+            signalStr = "44 "
 
         Ten = str(self.leftTime // 10 + 30)
         One = str(self.leftTime % 10 + 30)
-        sendStr = defaultStr + TRF + "30 30 31 "+ signalStr + Ten + " " + One
+        sendStr = defaultStr + SUB + "30 30 31 "+ signalStr + Ten + " " + One
         os.system(sendStr)
 
     def changeTurn(self):
-        if self.getSignal() == "R":
-            self.setSignal("G")
-        elif self.getSignal() == "G":
-            self.setSignal("R")
+        if self.getSignal() == "U":
+            self.setSignal("D")
+        elif self.getSignal() == "D":
+            self.setSignal("U")
         else:
             exit(1)
-        self.leftTime = 60
+        self.leftTime = 12
 
-    def afterOneSec(self):
+    def afterOneMin(self):
+        time.sleep(60)
         if self.leftTime > 0:
             self.leftTime -= 1
         else:
@@ -64,8 +62,7 @@ def main():
     while True:
         # os.system("sudo hcitool -i hci0 cmd 0x08 0x0008 13 02 01 06 03 03 aa fe 0b 16 aa fe 10 00 74 72 66 52 36 30 00 00 00 00 00 00 00 00 00 00 00 00")
         trafficObj.trafficBroadcasting()
-        time.sleep(1)
-        trafficObj.afterOneSec()
+        trafficObj.afterOneMin()
 
 
 if __name__ == "__main__":
