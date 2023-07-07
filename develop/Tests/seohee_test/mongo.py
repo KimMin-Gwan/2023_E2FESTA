0d95db90ad82c038e6bc83fc4bf3f5dcc0620dbd
seohee <shp6788@naver.com>
2023-07-07 15:55:43 +0900

    mongodb and yolov7 streaming
    practice mongodb
    practice camera streaming with yolov7

---
+++ develop/Tests/seohee_test/mongo.py
@@ -0,0 +1,47 @@
+from pymongo import MongoClient# pymongo 임포트
+#import datetime
+
+client=MongoClient('mongodb+srv://seohee:Qlqjs486@cluster.mb1ux71.mongodb.net/')
+
+db = client.hobby# 데이터베이스 이름 : hobby
+
+#print(client.list_database_names())#왜 config 안나오지?
+#print(client.dbdb.list_collection_names())
+
+ 
+
+# 삽입 ----------------------------------------------
+#data = {
+#    'author' : 'hun',
+#    'text' : 'mongoDB is what..?',
+#    'tags' : ['mongoDB', 'python', 'pymongo']
+#}
+#dpInsert = db.posts.insert_one(data)
+
+#data = {
+#    'author' : 'lee',
+#    'text' : 'mongoDB here',
+#    'tags' : ['mongoDB', 'python', 'pymongo']
+#}
+#dpInsert = db.posts.insert_one(data)
+
+# 읽기 -----------------------------------------------
+#for d in db['posts'].find():
+#    print(type(d))
+#    print(d['author'], d['text'], d['tags'])
+
+# 'author':'hun'인 데이터 조회
+#print(db.posts.find_one({'author':'hun'})['text'])
+
+# text 칼럼을 제외하고 데이터 가져오기
+#for d in db['posts'].find({}, {'text' : 0}):
+#    print(d)
+
+
+# 수정 ---------------------------------------------------------
+#db.posts.update_one({'author':'lee'},{'$set':{'author':'LEE'}})
+
+#삭제 --------------------------------------------------------
+#db.posts.delete_one({'author':'LEE'})
+
+client.close()
