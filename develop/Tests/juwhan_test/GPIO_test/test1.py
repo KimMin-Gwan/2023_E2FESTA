e7ecad725fa87b0b0179c5102abefc207ea4119f
sunjuwhan <mathsun0011@gmail.com>
2023-07-10 17:49:06 +0900

    Update test1.py

--- develop/Tests/juwhan_test/GPIO_test/test1.py
+++ develop/Tests/juwhan_test/GPIO_test/test1.py
@@ -1,21 +1,3 @@
-# import RPi.GPIO as GPIO 
-# import time
-
-# # 사용할 GPIO 핀의 번호를 설정
-# button_pin = 15
- 
-# GPIO.setwarnings(False) 
-# GPIO.setmode(GPIO.BCM) #핀모드 설정
-
-# # 버튼 핀의 입력설정 , PULL DOWN 설정 
-# GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) 
-
-# while 1: 
-#     if GPIO.input(button_pin) == GPIO.HIGH:
-#         print("Button pushed!")    
-#     time.sleep(0.1) 
-
-
 import RPi.GPIO as GPIO
 import time
 
@@ -28,11 +10,28 @@ GPIO.setmode(GPIO.BCM)
 # 버튼 핀 입력으로 설정 및 풀업 저항 적용
 GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def button_callback(channel):
-    print("Button Pressed")
+# 버튼 상태 변수 초기화
+button_pressed_time = None
+
+def button_pressed(channel):
+    global button_pressed_time
+    if GPIO.input(button_pin) == GPIO.LOW:
+        button_pressed_time = time.time()
+
+def button_released(channel):
+    global button_pressed_time
+    if button_pressed_time is not None:
+        button_released_time = time.time()
+        button_duration = button_released_time - button_pressed_time
+        if button_duration >= 3:
+            print("no")
+        else:
+            print("yes")
+        button_pressed_time = None
 
 # GPIO 이벤트 핸들러 등록
-GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
+GPIO.add_event_detect(button_pin, GPIO.BOTH, callback=button_pressed)
+GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=button_released, bouncetime=300)
 
 try:
     while True:
