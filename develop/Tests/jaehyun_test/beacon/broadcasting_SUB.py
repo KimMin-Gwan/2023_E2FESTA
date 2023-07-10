7c992b023873ee72422ddf0b6594cdb1c056d7ed
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2023-07-10 03:37:20 -0400

    Update broadcasting_SUB.py

--- develop/Tests/jaehyun_test/beacon/broadcasting_SUB.py
+++ develop/Tests/jaehyun_test/beacon/broadcasting_SUB.py
@@ -1,72 +1,63 @@
 """
-* Project : 2023CDP Eddystone Broadcasting Subway ver
+* Project : 2023CDP Eddystone Broadcasting
 * Program Purpose and Features :
 * - send broadcasting message
 * Author : JH KIM, JH SUN
-* First Write Date : 2023.07.04
+* First Write Date : 2023.07.03
 * ==========================================================================
 * Program history
 * ==========================================================================
 * Author    		Date		    Version		History
-* JH KIM            2023.07.04      v1.00       First Write
-* JH KIM            2023.07.07      v1.01       Broadcasting duration changed
+* JH KIM            2023.07.03      v1.00       First Write
+* JH KIM            2023.07.03      v1.01       add init code
+* JH KIM            2023.07.03      v1.02       creat class
+* JH KIM            2023.07.04      v1.03       TRF code modified/start time change 60->30
+* JH KIM            2023.07.07      v1.04       performance improvement
 """
 import os
 import time
-
 class trafficSignal:
     def __init__(self):
         os.system("sudo hciconfig hci0 up")
         os.system("sudo hciconfig hci0 leadv 3")
-        self.signal = "U"
-        self.leftTime = 6
-        self.leftTimeSec = 59
-        
+        self.signal = "R"
+        self.leftTime = 30
+
     def setSignal(self, newSig):
         self.signal = newSig
-        
+
     def getSignal(self):
         return self.signal
     
     def afterOneSec(self):
         time.sleep(1)
-        if self.leftTimeSec > 0:
-            self.leftTimeSec -= 1
-        else:
-            self.afterOneMin()
-    
-    def afterOneMin(self):
         if self.leftTime > 0:
             self.leftTime -= 1
         else:
             self.changeTurn()
-        self.leftTimeSec = 59
-            
+    
     def trafficBroadcasting(self):
         defaultStr = "sudo hcitool -i hci0 cmd 0x08 0x0008 17 02 01 06 03 03 aa fe 0f 16 aa fe 10 00 "
-        SUB = "53 55 42 "
-        if self.getSignal() == "U":
-            signalStr = "55 "
+        TRF = "54 52 46 "
+        if self.getSignal() == "G":
+            signalStr = "47 "
         else:
-            signalStr = "44 "
+            signalStr = "52 "
 
         Ten = str(self.leftTime // 10 + 30)
         One = str(self.leftTime % 10 + 30)
-        sendStr = defaultStr + SUB + "30 30 31 "+ signalStr + Ten + " " + One
+        sendStr = defaultStr + TRF + "30 30 31 "+ signalStr + Ten + " " + One
         os.system(sendStr)
         self.afterOneSec()
-        
 
     def changeTurn(self):
-        if self.getSignal() == "U":
-            self.setSignal("D")
-        elif self.getSignal() == "D":
-            self.setSignal("U")
+        if self.getSignal() == "R":
+            self.setSignal("G")
+        elif self.getSignal() == "G":
+            self.setSignal("R")
         else:
             exit(1)
-        self.leftTime = 12
-
-
+        self.leftTime = 60
 
 
 def main():
