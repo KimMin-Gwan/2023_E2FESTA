bd9f5555e10201bd2a727e0f19eac0fb54ea1620
김민관 <alsrhks2508@naver.com>
2023-07-11 02:25:15 +0900

    서버 패키지 초기 생성

---
+++ develop/modules/Server/server_master.py
@@ -0,0 +1,21 @@
+#server_master.py
+"""
+* Program Purpose and Features :
+* - Server_Master 
+* - This file will run in AWS
+* Author : MG KIM
+* First Write Date : 2023.07.11
+* ==========================================================================
+* Program history
+* ==========================================================================
+* Author    		Date		    Version		History                                                                                 code to fix
+* MG KIM			2023.07.11      v0.10	    make file
+"""
+
+class Server_Master:
+    # 생성자
+    def __init__(self):
+        pass
+
+    def __call__(self):
+        pass
