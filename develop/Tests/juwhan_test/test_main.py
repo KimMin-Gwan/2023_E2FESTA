91d9a24e7e6ede690aa5d14ee1c6e08314779c9e
sunjuwhan <mathsun0011@gmail.com>
2023-07-11 20:26:07 +0900

    Update test_main.py

--- develop/Tests/juwhan_test/test_main.py
+++ develop/Tests/juwhan_test/test_main.py
@@ -30,6 +30,7 @@ def infrasearch():
 if __name__=="__main__":
     main()
     
+
     scan_thread=threading.Thread(target=infrasearch)  #scan스레드
     print_thread=threading.Thread(target=print_a)  #scan후 처리할 스레드 시작
 
