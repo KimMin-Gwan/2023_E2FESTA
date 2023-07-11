263833b16799868f67fc04970b1daf2d69f6b64e
sunjuwhan <mathsun0011@gmail.com>
2023-07-11 19:30:14 +0900

    Create test_main.py

---
+++ develop/test_main.py
@@ -0,0 +1,25 @@
+
+from develop.modules.InfraSearch import *
+
+
+def main():
+    infrasearch()
+    
+def infrasearch():
+    duration =3 
+    scan_delegate = ScanDelegate()
+    scanner = Scanner().withDelegate(scan_delegate)
+    master=beacon_master(scanner,duration)
+    
+    while(True):
+        a=input("스캔을 원하시면 1을 입력하세요")
+        if a==1:
+            master.scan_beacon()
+            master.process_beacon()
+        else:
+            continue
+    
+
+if __name__=="__main__":
+    main()
+    
