5c90585fc9d5740fb0d37c3d636f9b4bb43cd27d
sunjuwhan <mathsun0011@gmail.com>
2023-07-11 20:27:24 +0900

    Update test_main.py

--- develop/test_main.py
+++ develop/test_main.py
@@ -1,7 +1,14 @@
 
 from modules.InfraSearch import *
+import threading
+import time
+que=PriorityQueue()
 
 
+def print_a():
+    while True:
+        print("a")
+        time.sleep(0.1)
 def main():
     infrasearch()
     
@@ -13,15 +20,23 @@ def infrasearch():
     
     while(True):
         a=input("스캔을 원하시면 1을 입력하세요")
-        if a=="1":
+        if a==1:
             master.scan_beacon()
             master.process_beacon()
         else:
             continue
-    
-
-
+        time.sleep(0.1)
 
 if __name__=="__main__":
     main()
+    
+
+    scan_thread=threading.Thread(target=infrasearch)  #scan스레드
+    print_thread=threading.Thread(target=print_a)  #scan후 처리할 스레드 시작
+
+    scan_thread.start()
+    print_thread.start()
+
+    scan_thread.join()
+    print_thread.join()
     
