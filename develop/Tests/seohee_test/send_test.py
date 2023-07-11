48be8037afe01d1d4546bdc07fd9e4ca57796014
seohee <108114351+sseoh47@users.noreply.github.com>
2023-07-12 01:05:12 +0900

    server(proto)

---
+++ develop/Tests/seohee_test/send_test.py
@@ -0,0 +1,9 @@
+import requests
+from flask import jsonify
+
+url='http://127.0.0.1:7777/rcv'
+post={'id':'20'}
+#resp=jsonify(post)
+respone=requests.post(url,json=post)
+print(respone)
+#print(respone.json())
