6ce675c933060b8538c24ae44107e37c61de4e64
ANTL-KJH <wogus9190@gmail.com>
2023-07-17 20:00:21 +0900

    Update test_main_kjh.py

--- develop/test_main_kjh.py
+++ develop/test_main_kjh.py
@@ -13,17 +13,22 @@
 import sys
 import time
 import threading
+
 sys.path.append('/home/pi/2023_E2FESTA')
 from develop.modules.button import *
 from develop.modules.InfraSearch import *
 from modules.Speaker import *
 
+
 class information:
     def __init__(self):
         self.__buttonState = -1
+        self.cs = threading.Lock()
 
     def setButtonState(self, state):
+        self.cs.acquire()
         self.__buttonState = state
+        self.cs.release()
 
     def getButtonState(self):
         return self.__buttonState
@@ -33,12 +38,13 @@ def runButton(button):
     while True:
         button.buttonInput()
 
+
 def runInfrasearch():
     duration = 2
     scan_delegate = ScanDelegate()
     scanner = Scanner().withDelegate(scan_delegate)
-    master=beacon_master(scanner,duration)
-    speaker=SpeakMaster()
+    master = beacon_master(scanner, duration)
+    speaker = SpeakMaster()
     master.scan_beacon()
     master.process_beacon()
     speaker.set_txt(master.get_gtts_data())
@@ -52,14 +58,18 @@ def main():
     button_thread.start()
     while True:
         time.sleep(0.1)
+        info.cs.acquire()
+        buttonState = info.getButtonState()
+        info.cs.release()
         print(info.getButtonState())
-        if info.getButtonState() == SCAN:
+        if buttonState == SCAN:
             info.setButtonState(-1)
             runInfrasearch()
-        elif info.getButtonState() == HANDCAM:
+        elif buttonState == HANDCAM:
             break
     button_thread.join()
     return
+
 
 if __name__ == "__main__":
     main()
