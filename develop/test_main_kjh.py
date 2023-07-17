74f59082303f7034a8d021afe50bb18f660ba2ed
JAEHYUN KIM <119037634+ANTL-KJH@users.noreply.github.com>
2023-07-17 03:12:27 -0400

    Update test_main_kjh.py

--- develop/test_main_kjh.py
+++ develop/test_main_kjh.py
@@ -3,13 +3,15 @@
 * Program Purpose and Features :
 * - Recognize User Button input
 * Author : JH KIM
-* First Write Date : 2023.07.10
+* First Write Date : 2023.07.17
 * ==========================================================================
 * Program history
 * ==========================================================================
 * Author    		Date		Version		History
 * JH KIM            2023.07.17		v1.00		First Write
 """
+import sys
+sys.path.append('/home/pi/2023_E2FESTA')
 from develop.modules.button import *
 
 class information:
@@ -18,11 +20,13 @@ class information:
 
     def setButtonState(self, state):
         self.__buttonState = state
-
+    def getButtonState(self):
+        return self.__buttonState
 def main():
     info = information()
     button = Button(info)
     button.info.setButtonState(1)
+    print(info.getButtonState())
 
 if __name__ == "__main__":
     main()
