f00015d4ea5725c5ea0ae52514451de68e690bc4
sunjuwhan <mathsun0011@gmail.com>
2023-07-17 15:18:05 +0900

    mongo
    only get key

--- develop/Tests/juwhan_test/server_class/master.py
+++ develop/Tests/juwhan_test/server_class/master.py
@@ -22,6 +22,7 @@ class Server:
 
         @self.app.route('/rcv',methods=['GET'])#http 로 접속시에는 127.0.0.1:8000/rcv?id=123&id=abc&id=... 하면 id값이 123이 들어가게된다.
         def get():
+            
             #넣어줘야할 데이터는 http://127.0.0.1:8000/rcv?id=ID&id=SUB&id=여기에 이제 key값을 넣어서 몇번째 비콘인지 찾는거지
             #현재 data frame 이 {'_id': ObjectId('64b394a993c1322d8377cefa'), 'ID': 'SUB', 'KEY': '1', 'DATA_1': '영남대방향'}
             #이런 형태로 저장되어있음
@@ -32,9 +33,7 @@ class Server:
             # db = client['test_sun']
             # collection = db['test']
             
-            
-            
-            query = {ids[0]:ids[1]}
+            query = {ids[0]:ids[1],"KEY":ids[2]}
             result = self.collection.find(query)
             list_reuslt=[]
             check_result=[]
