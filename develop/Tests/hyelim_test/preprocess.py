895441744c7281bf3cfa34922793759f5b8aba03
Hyelim <118596886+hyellm@users.noreply.github.com>
2023-07-20 21:09:11 +0900

    Update preprocess.py

--- develop/Tests/hyelim_test/preprocess.py
+++ develop/Tests/hyelim_test/preprocess.py
@@ -7,10 +7,19 @@ test = {'필기체' : 'htr/word_check', '인쇄체' : 'ocr_test/word_check',
         '증강인쇄체' : 'print/check', '간판' : 'Text'}
 for key, value in test.items():
     ocr_files = os.listdir(FILE_PATH + value)
-    print(ocr_files)
+
+    if key is '증강인쇄체' or key is '간판':
+        ocr_sub_files = os.listdir(FILE_PATH + value)
+
+        for arg in ocr_sub_files:
+            ocr_files = os.listdir(FILE_PATH + value + arg)
+            
+            print(ocr_files[0:10])
+
+    print(ocr_files[0:10])
+
 
 exit()
-
 
 def save_file(file, train_annotations, validation_annotations, test_annotations):
     with open('train_annotation.json', 'w',encoding='UTF-8') as file:
