ad9b097064947e6b2f42f6823799346211109d5b
seohee <108114351+sseoh47@users.noreply.github.com>
2023-07-20 15:55:15 +0900

    server

--- develop/Tests/juwhan_test/server_class/master.py
+++ develop/Tests/juwhan_test/server_class/master.py
@@ -66,7 +66,7 @@ class Server:
         
         
     def strat_server(self):
-        self.app.run(host="0.0.0.0", port="8000")
+        self.app.run(host="0.0.0.0", port="5000")
 
 
 if __name__=="__main__":
