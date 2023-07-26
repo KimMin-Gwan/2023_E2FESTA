540702a88d67468722474f38f6961951026a00d5
ANTL-KJH <wogus9190@gmail.com>
2023-07-26 15:40:47 +0900

    Update infra_master.py

--- develop/modules/InfraSearch/infra_master.py
+++ develop/modules/InfraSearch/infra_master.py
@@ -26,8 +26,8 @@ import threading
 
 class beacon_master:
     def __init__(self, Speaker, mainInfo) -> None:
-        self.receive = ReceiveSignal(scanner, duration)  #Receive class 
-        self.process = 0  
+        self.receive = ReceiveSignal(scanner, duration)
+        self.process = 0
         self.information = {}
         self.key = ""
         self.flag = ""
@@ -40,49 +40,47 @@ class beacon_master:
 
     def scan_beacon(self):  # scan부분
         self.information = self.receive.scanData()  # scan을 한뒤 이러한 데이터가 있음을 알려주고 data를 전달받는다.
-        
         if not self.information:  # 주변에 비콘이없다면
             self.data = "주변에 스캔된 비콘이 없습니다."
-            # speaker_thread = threading.Thread(target=self.start_gtts)   #스피커 스레드 생성
-            # speaker_thread.start() #스피커 스레드 
-            self.start_gtts()  #test code
+            # self.start_gtts()
+            speaker_thread = threading.Thread(target=self.start_gtts)
+            speaker_thread.start()
             return False
         else:
-            self.scan_result_gtts()   #주변 scan된 비콘 정보들 출력하기 위함
-            
-            for dict_key in self.information.keys():  #모든 받아온 beacon을 하나씩 차례로 꺼내와서 사용한다.
+            self.scan_result_gtts()
+            # self.start_gtts()
+
+            for dict_key in self.information.keys():
                 if dict_key == Subway:
                     self.data = "지하철"
                 elif dict_key == Traffic:
                     self.data = "신호등"
-                    
-                # speaker_thread = threading.Thread(target=self.start_gtts)
-                # speaker_thread.start()
-                self.start_gtts()  #test code
+                speaker_thread = threading.Thread(target=self.start_gtts)
+                speaker_thread.start()
+                # self.start_gtts()
                 sTime = time.time()
                 while True:
                     eTime = time.time()
                     if eTime - sTime > 3:
-                        break #3초이상 버튼 입력이 없을시 다음 beacon으로 넘어간다.
+                        break
                     if self.mainInfo.getButtonState() == 2:
+                        print("Here 2")
                         self.speaker.setSpeakerFlag(1)
                         time.sleep(0.01)
-                        self.flag = dict_key  #flag를 지정해서 
+                        self.flag = dict_key
                         self.mainInfo.setButtonState(-1)
-                        # if speaker_thread.is_alive():  #스피커 스레드가 살아있다면 끝날때 까지 기다린다.
-                        #     speaker_thread.join()
+                        if speaker_thread.is_alive():
+                            speaker_thread.join()
                         return True
-                    
-                    
             self.data = "버튼이 입력되지 않았습니다."
-            # speaker_thread = threading.Thread(target=self.start_gtts)
-            # speaker_thread.start()
-            self.start_gtts()  #test code
+            speaker_thread = threading.Thread(target=self.start_gtts)
+            speaker_thread.start()
+            # self.start_gtts()
             return False
 
     def process_beacon(self):  # processes하는 부분이다.
         self.process = ProcessingData(self.information, self.flag)  # ProcessingData클래스에 인자전달과 생성을 해준다
-        self.process.process_beacon_data()  #비콘에 맞게끔 데이터를 처리해주고
+        self.process.process_beacon_data()
 
     def get_gtts_data(self):
         self.data, self.flag, self.key = self.process.return_gtts_mssage()  # gtts 데이터를 return해준다.
@@ -90,14 +88,12 @@ class beacon_master:
     def send_server(self):
         url = 'http://43.201.213.223:8080/rcv?id=ID&id=' + self.flag + '&id=' + self.key  # server로 전달할 id이다.
         response = requests.get(url)
-        self.data = response.text+self.data
+        self.data = response.text + self.data
 
         print("확인할 최종 data======================================", self.data)
 
     def start_gtts(self):
-        speaker_thread = threading.Thread(target=self.speaker.tts_read,args=(self.data,))
-        # self.speaker.tts_read(self.data)
-        speaker_thread.start()
+        self.speaker.tts_read(self.data)
         self.data = ""  # 항상 읽고 data는 초기화 시켜준다.
 
     def connect_data_base(self):
@@ -107,22 +103,21 @@ class beacon_master:
 
     def scan_result_gtts(self):
         result = []
-        
+
         self.data = "주변에 "
 
         for i in self.information.keys():
             if i == Traffic:
-                self.data += (trf_gtts+", ")
+                self.data += (trf_gtts + ", ")
 
             elif i == Subway:
-                self.data += (sub_gtts+", ")
+                self.data += (sub_gtts + ", ")
         self.data = self.data[:-2]
 
         self.data += "이 있습니다. 원하시는 정보에 예 버튼을 눌러주세요"
         print(self.data)
-        # speaker_thread = threading.Thread(target=self.start_gtts, args=())
-        # speaker_thread.start()
-        self.start_gtts()
+        speaker_thread = threading.Thread(target=self.start_gtts(), args=())
+        speaker_thread.start()
         self.mainInfo.setButtonState(-1)
 
     def runScanBeacon(self):
@@ -130,7 +125,7 @@ class beacon_master:
         state = self.scan_beacon()
         if (state == True):  # 주변에 scan된 비콘이있을때
             self.process_beacon()
-            self.connect_data_base() #데이터 베이스에
+            self.connect_data_base()
             return
         else:
             return
