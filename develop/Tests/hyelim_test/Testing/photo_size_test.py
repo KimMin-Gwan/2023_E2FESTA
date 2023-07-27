0fe4279b2b32b6ab6ee9ad89d07dabfed29d3072
hyellm <yhl9779@naver.com>
2023-07-27 20:38:21 +0900

    Create photo_size_test.py

---
+++ develop/Tests/hyelim_test/Testing/photo_size_test.py
@@ -0,0 +1,21 @@
+# import cv2
+# import os
+ 
+# path = 'C:/Users/y2h75/Desktop/hyelim/# 공모전/2023_E2FESTA/500651.jpg'
+# imgName = 'photoo'
+ 
+# img = cv2.imread(os.path.join(path, imgName))
+# print(img.shape) # h, w, c
+
+import cv2
+import os
+import time
+
+path = 'C:/Users/y2h75/Desktop/hyelim/'
+imgName = '500651.jpg'
+
+start_time = time.time()
+img = cv2.imread(os.path.join(path, imgName))
+end_time = time.time()
+print(end_time - start_time)
+print(img.shape) # h, w, c
