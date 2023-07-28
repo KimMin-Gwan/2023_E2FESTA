272c7b4f31100408ff8af3fa88793db1a21a21e9
sunjuwhan <mathsun0011@gmail.com>
2023-07-28 17:33:02 +0900

    Update cameratest.py

--- develop/cameratest.py
+++ develop/cameratest.py
@@ -1,76 +1,33 @@
-## License: Apache 2.0. See LICENSE file in root directory.
-## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
-
-###############################################
-##      Open CV and Numpy integration        ##
-###############################################
-
 import pyrealsense2 as rs
-import numpy as np
-import cv2
 
-# Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
+def main():
+    # 카메라 초기화
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)  # 해상도와 프레임 속도 설정
 
-# Get device product line for setting a supporting resolution
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
-device_product_line = str(device.get_info(rs.camera_info.product_line))
+    # 스트리밍 시작
+    pipeline.start(config)
 
-found_rgb = False
-for s in device.sensors:
-    if s.get_info(rs.camera_info.name) == 'RGB Camera':
-        found_rgb = True
-        break
-if not found_rgb:
-    print("The demo requires Depth camera with Color sensor")
-    exit(0)
+    try:
+        while True:
+            # 프레임 얻기
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
 
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+            # 프레임이 유효한지 확인
+            if not depth_frame:
+                continue
 
-if device_product_line == 'L500':
-    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-else:
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+            # 프레임 데이터 얻기
+            distance = depth_frame.get_distance(640, 360)  # 중앙점의 거리를 얻음 (x, y)
 
-# Start streaming
-pipeline.start(config)
+            # 출력
+            print(f"거리: {distance:.2f} 미터")
 
-try:
-    while True:
+    finally:
+        # 종료 시 리소스 해제
+        pipeline.stop()
 
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
-
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
-
-        # If depth and color resolutions are different, resize color image to match depth image for display
-        if depth_colormap_dim != color_colormap_dim:
-            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-            images = np.hstack((resized_color_image, depth_colormap))
-        else:
-            images = np.hstack((color_image, depth_colormap))
-
-        # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
-
-finally:
-
-    # Stop streaming
-    pipeline.stop()
+if __name__ == "__main__":
+    main()
