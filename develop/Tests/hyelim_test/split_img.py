a86c515bf5dd4e37771604a5cc7ad453b0745422
Seungjun07 <121211597+Seungjun07@users.noreply.github.com>
2023-07-31 21:57:02 +0900

    Create split_img.py

---
+++ develop/Tests/hyelim_test/split_img.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import json
+import shutil
+
+PATH = "D:/kor_dataset/write/dataset/"
+DEST_PATH = "D:/kor_dataset/dataset/"
+JSON_FILE = {'train' : "D:/kor_dataset/write/train_annotation.json",
+             'validation' : "D:/kor_dataset/write/validation_annotation.json",
+             'test' : "D:/kor_dataset/write/test_annotation.json"}
+
+#img_file = os.listdir(PATH)
+
+for TYPE, json_path in JSON_FILE.items():
+    print(TYPE, " Working")
+
+    with open(json_path, 'r', encoding='UTF-8') as f:
+        json_list = json.load(f)
+
+    for idx, file_name in enumerate(json_list.keys()):
+        if idx % 1000 == 0:
+            print({idx}, " / ", len(json_list))
+
+        shutil.move(PATH + file_name,
+                    DEST_PATH + "/"+ TYPE + "/"+ file_name)
+        
+
+print("end of process")
+
+    
+    
+
