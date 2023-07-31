7030db2fe42f6c33448c3d1b34a9c958541002a8
hyellm <yhl9779@naver.com>
2023-07-31 22:01:04 +0900

    camera

--- develop/Tests/hyelim_test/camera_on_test_Class.py
+++ develop/Tests/hyelim_test/camera_on_test_Class.py
@@ -1,7 +1,7 @@
 import cv2
+import numpy as np
 # import shutil
 # import time
-
 
 webcam = cv2.VideoCapture(0)  # 0번 카메라
 # PATH = 'C:/Users/y2h75/Desktop/hyelim/# 공모전/2023_E2FESTA'
@@ -19,7 +19,7 @@ class camera():
         while webcam.isOpened():  # 카메라가 켜졌을 때
             self.status, self.frame = webcam.read()
             if self.status:
-                cv2.imshow("test", self.frame)  # 창 제목
+                cv2.imshow("Camera", self.frame)  # 창 제목
     
             if cv2.waitKey(1) & 0xFF == ord('q'):  # q 누르면 나가기
                 break
@@ -29,41 +29,24 @@ class camera():
                 # current_dir = os.path.dirname(os.path.abspath(__file__))
                 # image_path = os.path.join(current_dir, 'self_camera_test.jpg')
                 # cv2.imwrite(image_path, self.frame)  # 사진 저장
-                cv2.imwrite('./selfcameratest.jpg', self.frame) # 사진 저장
+                # cv2.imwrite('./selfcameratest.jpg', self.frame) # 사진 저장
+                
+                # 여기에서 frame을 원하는 로직에 따라 처리합니다.
+                # 이 예시에서는 frame을 그대로 사용하겠습니다.
+
+
+                # 행렬로 처리된 프레임을 변수에 할당
+                processed_frame_array = self.frame
+                
+                break
+            
+                # 화면에 출력 (이미지를 저장하지 않고 화면에도 표시)
+                # cv2.imshow("Processed Frame", processed_frame_array)
+                
                 # shutil.move(PATH + '/selfcameratest.jpg', PATH + '/develop/Tests/hyelim_test/selfcameratest.jpg') # (기존 폴더, 옮길 폴더)
                 # print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
 
-
+        print(processed_frame_array)
+        
         webcam.release()
         cv2.destroyAllWindows()
-        
-#################################################################
-
-# import cv2
-
-# webcam = cv2.VideoCapture(0)  # 0번 카메라
-
-# class camera():
-#     def __init__(self):
-#         #self.webcam = cv2.VideoCapture(0)  # 0번 카메라
-#         print("make camera")
-        
-#     def StartHandCam(self):
-#         if not webcam.isOpened():  # 카메라가 켜지지 않았을 때
-#             print("Could not open webcam")  # 오류 메시지 출력
-#             exit()  # 종료
-
-#         while webcam.isOpened():  # 카메라가 켜졌을 때
-#             self.status, self.frame = webcam.read()
-        
-#             if self.status:
-#                 cv2.imshow("test", self.frame)  # 창 제목
-    
-#     # i       if cv2.waitKey(1) & 0xFF == ord('q'):  # q 누르면 나가기
-#     #             break
-    
-#     #         if cv2.waitKey(1) & 0xFF == ord('a'):  # a 누르면 사진 찍기
-#     #             cv2.imwrite('self camera test.jpg', self.frame) # 사진 저장
-                
-#         webcam.release()
-#         cv2.destroyAllWindows()
