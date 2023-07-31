446aaa3e54c9a4b8ea721f91958279035e1586cd
seohee <108114351+sseoh47@users.noreply.github.com>
2023-07-31 14:40:39 +0900

    file move

--- develop/modules/Monitoring/monitor.py
+++ develop/modules/Monitoring/monitor.py
@@ -1,48 +1,60 @@
 from flask import Flask, render_template, Response
-from flask import send_file, json, make_response #인프라 서치에서 한국어 반환 위해
-import cv2 #핸드카메라 위해
-import io #스냅샷 위해
+from flask import send_file #인프라 서치에서 한국어 반환 위해
+import cv2, io #핸드카메라&스냅샷 위해
+from Monitoring import SUB,BUS,TRAFT
 
-app=Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
-streaming=True
-stop_frame=None
+class Monitor:
 
-def hand_cam():
-    camera=cv2.VideoCapture(0) #0번캠(현재 내 카메라)
+    def __init__(self):
+        self.app=Flask(__name__)
+        self.app.config['JSON_AS_ASCII'] = False
+        self.streaming=True
+        self.stop_frame=None
+
+        self.route()
+
     
-    while (streaming):
+    def hand_cam(self):
+
+        camera=cv2.VideoCapture(0) #0번캠(현재 내 카메라)
+    
+        while (self.streaming):
         # 프레임 단위로 캡쳐
-        success, frame = camera.read()  #카메라 프레임 읽어오기      
+            success, frame = camera.read()  #카메라 프레임 읽어오기      
 
-        if (not success):
-            break
+            if (not success):
+                break
 
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            global stop_frame
-            stop_frame=frame
+            else:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                global stop_frame
+                stop_frame=frame
+           
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            #프레임 하나씩 보여준다
+                #프레임 하나씩 보여준다
+
+    def route(self):
+        @self.app.route('/snapshot')
+        def snapshot():
+            return send_file(io.BytesIO(stop_frame), mimetype='image/jpeg')
 
 
-@app.route('/snapshot')
-def snapshot():
-    return send_file(io.BytesIO(stop_frame), mimetype='image/jpeg')
+        @self.app.route('/video_show')
+        def video_show():
+            return Response(self.hand_cam(), 
+                mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/video_show')
-def video_show():
-    return Response(hand_cam(), 
-        mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/')
-def hello_name():
-    sub='지하철'
-    bus='버스'
-    traft='신호등'
-    return render_template('index.html',name1=sub,name2=bus,name3=traft)
+        @self.app.route('/')
+        def hello_name():
+            return render_template('index.html',name1=SUB,name2=BUS,name3=TRAFT)
 
-if __name__=='__main__':
-    app.run(host="0.0.0.0", port="8080")
+    def start_monitor(self):
+        self.app.run(host="0.0.0.0", port="8080")
+
+
+if __name__=="__main__":
+    monitor=Monitor()
+    monitor.start_monitor()
