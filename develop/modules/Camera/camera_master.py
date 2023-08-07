f02c5c9faf43add164164b45cca867043f460ff0
seohee <108114351+sseoh47@users.noreply.github.com>
2023-08-07 14:55:13 +0900

    change function name

--- develop/modules/Camera/camera_master.py
+++ develop/modules/Camera/camera_master.py
@@ -2,18 +2,20 @@ import cv2
 import numpy as np
 
 webcam = cv2.VideoCapture(0)  # 0번 카메라
+# handcam = cv2.VideoCapture(1) # 1번 카메라
 
 class Camera():
     def __init__(self):
         print("make camera")
         
-    def StartHandCam(self):
+    def StartWebCam(self):
         if not webcam.isOpened():  # 카메라가 켜지지 않았을 때
             print("Could not open webcam")  # 오류 메시지 출력
             exit()  # 종료
 
         while webcam.isOpened():  # 카메라가 켜졌을 때
             self.status, self.frame = webcam.read()
+
             if self.status:
                 cv2.imshow("Camera", self.frame)  # 창 제목
     
@@ -49,7 +51,7 @@ class Camera():
 
 def main():
     camera=Camera()
-    camera.StartHandCam()
-    
+    camera.StartWebCam()
+
 if __name__ == "__main__":
     main()
