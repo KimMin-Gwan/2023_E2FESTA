b8bd04776771860bb00f445651236a97a8055177
hyellm <yhl9779@naver.com>
2023-08-07 14:58:34 +0900

    Update camera_master.py

--- develop/modules/Camera/camera_master.py
+++ develop/modules/Camera/camera_master.py
@@ -23,26 +23,8 @@ class Camera():
                 break
     
             if cv2.waitKey(1) & 0xFF == ord('a'):  # z 누르면 사진 찍기
-                # start = time.time()  # 시작 시간 저장
-                # current_dir = os.path.dirname(os.path.abspath(__file__))
-                # image_path = os.path.join(current_dir, 'self_camera_test.jpg')
-                # cv2.imwrite(image_path, self.frame)  # 사진 저장
-                # cv2.imwrite('./selfcameratest.jpg', self.frame) # 사진 저장
-                
-                # 여기에서 frame을 원하는 로직에 따라 처리합니다.
-                # 이 예시에서는 frame을 그대로 사용하겠습니다.
-
-
-                # 행렬로 처리된 프레임을 변수에 할당
-                processed_frame_array = self.frame
-                
+                processed_frame_array = self.frame  # 행렬로 처리된 프레임을 변수에 할당
                 break
-            
-                # 화면에 출력 (이미지를 저장하지 않고 화면에도 표시)
-                # cv2.imshow("Processed Frame", processed_frame_array)
-                
-                # shutil.move(PATH + '/selfcameratest.jpg', PATH + '/develop/Tests/hyelim_test/selfcameratest.jpg') # (기존 폴더, 옮길 폴더)
-                # print("time :", time.time() - start)  # 현재시각 - 시작시간 = 실행 시간
 
         print(processed_frame_array)
         
