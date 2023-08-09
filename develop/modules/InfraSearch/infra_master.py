58536a530903fdc2f18f9df61dd74272bbf81d80
ANTL-KJH <wogus9190@gmail.com>
2023-08-09 16:54:42 +0900

    Update infra_master.py

--- develop/modules/InfraSearch/infra_master.py
+++ develop/modules/InfraSearch/infra_master.py
@@ -12,6 +12,7 @@
 * MG KIM			2023.07.11      v0.10	    make from /juwhan_test/split_class.py
 * JH SUN            2023.07.18      v1.00       write beacon master
 * JH KIM            2023.07.20      v1.01       set_txt, read_tts merged
+* JH KIM            2023.08.09      v1.10       source code optimization
 """
 import time
 
@@ -34,35 +35,28 @@ class beacon_master:
         self.speaker = Speaker
         self.mainInfo = mainInfo
 
-
-    def __call__(self):
-        pass
-
     def scan_result_gtts(self):
-        result = []
         self.data = "주변에 "
         for i in self.information.keys():
-            if i == Traffic:
+            if i == Traffic:                    # 신호등
                 self.data += (trf_gtts + ", ")
-
-            elif i == Subway:
+            elif i == Subway:                   # 지하철
                 self.data += (sub_gtts + ", ")
         self.data = self.data[:-2]
         self.data += "이 있습니다. 원하시는 정보에 예 버튼을 눌러주세요"
         exitCode = self.start_gtts()
         return exitCode
 
-    def scan_beacon(self):  # scan부분
+    def scan_beacon(self):
         self.information = self.receive.scanData()  # scan을 한뒤 이러한 데이터가 있음을 알려주고 data를 전달받는다.
-        if not self.information:  # 주변에 비콘이없다면
+        if not self.information:                    # scan된 data가 없는 경우
             self.data = "주변에 스캔된 비콘이 없습니다."
             self.start_gtts()
             return False
         else:
             exitCode = self.scan_result_gtts()
-            if exitCode == -1:
+            if exitCode == -1:      # Infrasearch exit button input
                 return -1
-            # self.start_gtts()
 
             for dict_key in self.information.keys():
                 if dict_key == Subway:
@@ -70,9 +64,8 @@ class beacon_master:
                 elif dict_key == Traffic:
                     self.data = "신호등"
 
-                #self.speaker_thread.start()
                 exitCode = self.start_gtts()
-                if exitCode == -1:
+                if exitCode == -1:  # Infrasearch exit button input
                     return exitCode
                 sTime = time.time()
                 while True:
@@ -82,8 +75,6 @@ class beacon_master:
                     if self.mainInfo.getButtonState() == 2 or exitCode == 2:
                         self.mainInfo.setButtonState(-1)
                         self.flag = dict_key
-                        #if self.speaker_thread.is_alive():
-                        #    self.speaker_thread.join()
                         return True
             self.data = "버튼이 입력되지 않았습니다."
             self.start_gtts()
@@ -91,26 +82,23 @@ class beacon_master:
 
     def start_gtts(self):
         exitCode = self.speaker.tts_read(self.data)
-        self.data = ""  # 항상 읽고 data는 초기화 시켜준다.
+        self.data = ""  # data reset
         return exitCode
 
     def connect_data_base(self):
         url = 'http://43.201.213.223:8080/rcv?id=ID&id=' + self.flag + '&id=' + self.key  # server로 전달할 id이다.
         response = requests.get(url)
         self.data = response.text + self.data
-        print("확인할 최종 data======================================", self.data)
-
-
 
     def process_beacon(self):  # processes하는 부분이다.
         self.process = ProcessingData(self.information, self.flag)  # ProcessingData클래스에 인자전달과 생성을 해준다
         self.process.process_beacon_data()
-        # self.connect_data_base()       # data_base에 연결하는 경우 주석 해제
-        self.data, self.flag, self.key = self.process.return_gtts_mssage()  # gtts 데이터를 return해준다.
+        # self.connect_data_base()                                  # data_base에 연결하는 경우 주석 해제
+        self.data, self.flag, self.key = self.process.return_gtts_mssage()  # prcessing된 message return
 
     def runScanBeacon(self):
-        state = self.scan_beacon()
+        state = self.scan_beacon()          # beacon scan
         if (state == True):                 # 주변에 scan된 비콘이있을때
-            self.process_beacon()
-            self.start_gtts()
+            self.process_beacon()           # beacon data processing
+            self.start_gtts()               # speaker output
         return
