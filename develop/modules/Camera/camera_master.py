de3a679a8e730b601b894064473c2f7f7ec51fef
Hyelim <118596886+hyellm@users.noreply.github.com>
2023-08-11 22:25:20 +0900

    Update camera_master.py

--- develop/modules/Camera/camera_master.py
+++ develop/modules/Camera/camera_master.py
@@ -15,6 +15,7 @@
 * SH PARK			2023.08.07      v0.30	    set intel camera modul
 * SH PARK			2023.08.07      v0.31	    make test main file
 * MG KIM			2023.08.09      v0.40	    초기 설계 세팅 및 수정
+* HL YANG			2023.08.11      v0.50	    edit internal code
 """
 
 import cv2
@@ -30,45 +31,48 @@ class Camera_Master():
         # hand camera init
         self.info = info
         self.web_monitor = web_monitor
-        # cv2.CAP_DSHOW : 다이렉트 쇼
-        self.handcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # 0번 카메라
-        self.status = 1
+        self.handcam = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # 0번 카메라, cv2.CAP_DSHOW : 다이렉트 쇼
+        self.status = 1  # 1: Web, 2: Hand
+        self.swap_flag = 0  # 0: default, 1: for replacement
         
         # Configure depth and color streams
         self.pipeline = rs.pipeline()
         self.config = rs.config()
         # Enable RGB stream only
         self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-        
+    
+
+    # 카메라 ON
     def RunCamera(self):
-        
+        # webcam(기본값) 스레드 실행
         self.thread = threading.Thread(target=self.StartWebCam, args=(True))
         self.thread.start()
         return
-        
+    
+
+    # 카메라 전환 함수
     def swap_camera(self):
-        # 그냥 join 해버리니까 스레드 종료과정에서 while 부근에서 오류가남
-        # 그래서 플래그를 새워서 와일문을 종료시켜서 끌것
-        self.swap_flag = 1 
+        # 그냥 join 해버리니까 스레드 종료과정에서 StartCam 내부의 while 부근에서 오류가 남
+        # 그래서 플래그를 세워 StartCam 내부의 while 문을 종료시켜서 끌 것
+        self.swap_flag = 1  # 기본 flag == 0, 멈추려고 할 때는 flag == 1로 설정해주기
         cv2.destroyAllWindows()
         #self.thread.join()
-        if self.status == 1:
+
+        # WebCam → HandCam
+        if self.status == 1:  # Now: Web
             self.swap_flag = 0
             self.thread = threading.Thread(target=self.StartHandCam, args=(True))
             self.thread.start()
-            self.status = 2
+            self.status = 2  # Change Cam's status; web > hand
+        
+        # HandCam → WebCam
         else:
-            
             self.swap_flag = 0
             self.thread = threading.Thread(target=self.StartWebCam, args=(True))
             self.thread.start()
-            self.status = 1
-        
-    # 카ㅔㅁ라 바뀌는 함수.ㅋㅏ메라 바꿔.
-    #  : 
-    # self.thread.join()  # 이 스레드 정지
-    # thread2 = threading.Thread(target=self.StartHandCam, args=(True))
+            self.status = 1  # Change Cam's status; hand > web
     
+
     # flag 를 True로 하면 화면에 출력이 나옴
     def StartHandCam(self, flag = False):
 
@@ -78,8 +82,10 @@ class Camera_Master():
             exit()  # 종료
 
         while self.handcam.isOpened():  # 카메라가 켜졌을 때
-            if self.swap_flag == 1:
+
+            if self.swap_flag == 1:  # web에서 swap cam 버튼이 눌려 flag 0 > 1 변경, 카메라 전환을 하겠다는 의미
                 break
+
             ret, self.frame = self.handcam.read()
 
             if flag:
@@ -92,17 +98,15 @@ class Camera_Master():
                 print("Error : Camera did not captured")
                 continue
             
-            
-       
         self.handcam.release()
         # cv2.destroyAllWindows()
         # self.StartWebCam()
         
-        # 버튼 바꾸는 함수 실행해
+        # 버튼 바꾸는 함수 실행해 < 필요없을 것 같아서 지움
+        # self.swap_camera()
 
 
-
-
+    # Web cam ON
     def StartWebCam(self, flag = False):
         ## License: Apache 2.0. See LICENSE file in root directory.
         ## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
@@ -114,6 +118,10 @@ class Camera_Master():
         # Start streaming self.pipeline.start(self.config)
         try:
             while True:
+
+                if self.swap_flag == 1:  # web에서 swap cam 버튼이 눌려 flag 0 > 1 변경, 카메라 전환을 하겠다는 의미
+                break
+
                 # Wait for a frame : color
                 frames = self.pipeline.wait_for_frames()
                 color_frame = frames.get_color_frame()
@@ -123,27 +131,29 @@ class Camera_Master():
 
                 # Convert image to numpy array
                 self.frame = np.asanyarray(color_frame.get_data())
-                if flag:
+
+                if flag:  # flag == 1로 설정 시(기본값 0) window에 카메라 화면 창 띄우기
                     # Show RGB image
                     cv2.namedWindow('RGB Camera', cv2.WINDOW_AUTOSIZE)
                     cv2.imshow('RGB Camera', self.frame)
                     cv2.waitKey(1)
                     
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                    if cv2.waitKey(1) & 0xFF == ord('q'):  # q 키 누르면 카메라 창을 종료하도록 설정
                         break
                 
-                if self.web_monitor.get_swap_button():
+                if self.web_monitor.get_swap_button():  # web 화면에서 카메라 전환 버튼을 눌렀을 때 카메라 전환
                     self.swap_camera()
                     # break
                 
-            if flag:
+            if flag:  # flag == 1로 설정 시(기본값 0) window에 띄워진 카메라 화면 창 닫기
                 cv2.destroyAllWindows()
                 
-            self.StartHandCam(False)
+            self.StartHandCam(False)  # hand cam 실행
     
         finally:
             # Stop streaming
             self.pipeline.stop()
+       
        
     # 모니터링용 데이터 처리
     def get_frame(self):
