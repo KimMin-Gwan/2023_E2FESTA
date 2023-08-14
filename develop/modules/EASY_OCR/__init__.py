6394601d5f0c75c939a340813d87dca66b4c22f1
jun <121211597+Seungjun07@users.noreply.github.com>
2023-08-14 16:45:49 +0900

    update_EasyOCR

--- develop/modules/EASY_OCR/__init__.py
+++
@@ -1,2 +0,0 @@
-from modules.EASY_OCR.constant import *
-from modules.EASY_OCR.Easy_ocr import *
--- develop/modules/TextRecognition/Easy_ocr.py
+++ develop/modules/TextRecognition/Easy_ocr.py
@@ -1,5 +1,18 @@
-from modules.EASY_OCR.utils import *
-
+from matplotlib import pyplot as plt
+from imutils.perspective import four_point_transform
+from imutils.contours import sort_contours
+import imutils
+from easyocr import Reader
+import cv2
+import requests
+import numpy as np
+from PIL import ImageFont, ImageDraw, Image
+import time
+import copy
+import pickle
+import torch
+px=[-1,1,1,-1]
+py=[-1,-1,1,1]
 
 class easy_ocr:
     def __init__(self):   #생성자
