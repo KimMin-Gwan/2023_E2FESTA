cb4696d7ac7e963ce5faad6e2f3de91676f43d56
sunjuwhan <alsrhks2508@naver.com>
2023-08-21 18:07:05 +0900

    파이썬으로 경로수정

--- main/raspberry_pi/Camera/constant.py
+++ main/raspberry_pi/Camera/constant.py
@@ -1,3 +1,5 @@
 #TEST_FLAG = (True, )
 TEST_FLAG = (False, )
-WAIT_IMG_PATH = "D:/2023_E2FESTA/main/raspberry_pi/Camera/img/wait_img.png"
+#WAIT_IMG_PATH = "D:/2023_E2FESTA/main/raspberry_pi/Camera/img/wait_img.png"
+
+WAIT_IMG_PATH = "/home/pi/2023_E2FESTA/main/raspberry_pi/Camera/img/wait_img.png"
--- main/raspberry_pi/Object_detect/constant.py
+++ main/raspberry_pi/Object_detect/constant.py
@@ -1,5 +1,10 @@
-PATH_TO_MODEL='D:/2023_E2FESTA/main/raspberry_pi/object_detect/model/mobile_SSD_v2_320x320_kr_ob.tflite'   # Path to .tflite model file
-PATH_TO_LABEL='D:/2023_E2FESTA/main/raspberry_pi/object_detect/model/labelmap.txt'   # Path to labelmap.txt file
+#ATH_TO_MODEL='D:/2023_E2FESTA/main/raspberry_pi/object_detect/model/mobile_SSD_v2_320x320_kr_ob.tflite'   # Path to .tflite model file
+#PATH_TO_LABEL='D:/2023_E2FESTA/main/raspberry_pi/object_detect/model/labelmap.txt'   # Path to labelmap.txt file
+
+PATH_TO_MODEL = "/home/pi/2023_E2FESTA/main/raspberry_piobject_detect/model/mobile_SSD_v2_320x320_kr_ob.tflite"
+PATH_TO_LABEL = "/home/pi/2023_E2FESTA/main/raspberry_piobject_detect/model/labelmap.txt"
+
+
 MIN_CONF_THRESHOLD = 0.2
 INPUT_MEAN = 127.5
 INPUT_STD = 127.5
