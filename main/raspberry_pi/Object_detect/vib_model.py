ee02fbdebe25b4a00f833c921c1f6a3024bce7a4
sunjuwhan <mathsun0011@gmail.com>
2023-09-25 10:58:50 +0900

    Update vib_model.py

--- main/raspberry_pi/Object_detect/vib_model.py
+++ main/raspberry_pi/Object_detect/vib_model.py
@@ -25,7 +25,8 @@ class Vibrater:
     def give_vib_feedback(self, distances = [DIST_THRESHOLD+1]):
         while(True):
             # 최소거리에 바탕이된 진동 피드백 제공
-            distance = self.__find_min_dist(distances=distances)
+            #distance = self.__find_min_dist(distances=distances)
+            distance=DIST_THRESHOLD-100
             # 현재 사물의 최소 거리를 바탕으로 진동사이클 지정
             if (self.__check_distance(distance)):
                 GPIO.output(VIB_PIN, False)
