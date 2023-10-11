ebdbb9db8e3992d712d79c3337b639a08bf13490
sunjuwhan <mathsun0011@gmail.com>
2023-10-11 16:27:20 +0900

    ipconfig

--- main/raspberry_pi/Monitoring/constant.py
+++ main/raspberry_pi/Monitoring/constant.py
@@ -6,7 +6,9 @@ TRAFT = '신호등'
 JSON_AS_ASCII = 'JSON_AS_ASCII'
 JPG = '.jpg'
 #HOST = "0.0.0.0"
+
 #HOST = "192.168.50.13"
 #HOST="172.20.47.152"
-HOST="172.20.10.5"
+HOST="192.168.50.72"
+#HOST="172.20.10.5"  herim ip
 PORT = "7777"
