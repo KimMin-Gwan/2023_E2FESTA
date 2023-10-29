2bf0e72c43fe648769bb1b40579fd603354972d3
sunjuwhan <mathsun0011@gmail.com>
2023-10-29 13:32:43 +0900

    d

--- develop/Tests/juwhan_test/CAMERA/test.py
+++ develop/Tests/juwhan_test/CAMERA/test.py
@@ -41,6 +41,7 @@ with open('output.jpg', 'rb') as output_file:
 #d=image_bytes.flatten()
 #s=d.tostring()
 print(len(image_bytes))
+print(image_bytes[0:15])
 socket.sendto(image_bytes[0:15],(UDP_IP,UDP_PORT))
 
 
